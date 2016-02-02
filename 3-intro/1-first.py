ce5b0a0efc7a720b9d459d280f3f3cf8074cdb17
w3cp <jannat.books@gmail.com>
2016-02-02 03:05:55 +0000

    modify: 1-first.py

--- 3-intro/1-first.py
+++ 3-intro/1-first.py
@@ -1,7 +1,7 @@
 # for terminal - interactive mode only
 2 + 2
 50 - 5 * 6
-( 50 - 5 * 6) / 4
+(50 - 5 * 6) / 4
 8 / 5 # division always returns a floating point number
 17 / 3 # classic division returns a float
 17 // 3 # floor division discards the fractional part
@@ -20,4 +20,4 @@ price * tax
 price + _  
 # In interactive mode, the last printed expression is
 # assigned to the variable _ . 
-round (_, 2)
+round(_, 2)
